4f850cf67b857b61950677be25ba6b7b7514ac89
Shriom <shriomtripathi33@gmail.com>
2018-04-21 16:07:13 +0530

    1.Done adding FirebaseAuth UI 2.Both Google and Facebook LogIn is working.

--- app/src/main/java/com/example/joker/springlr/MainActivity.java
+++ app/src/main/java/com/example/joker/springlr/MainActivity.java
@@ -1,13 +1,75 @@
 package com.example.joker.springlr;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
+
+import com.firebase.ui.auth.AuthUI;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
+
+    //request code for signIn
+    private static final int RC_SIGN_IN = 123;
+
+    //authenticator
+    private FirebaseAuth firebaseAuth;
+
+    //auth state listener
+    private FirebaseAuth.AuthStateListener authStateListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+
+
+        authStateListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if(user != null){
+                    //user signed in
+                    Toast.makeText(MainActivity.this,"You have logged In.",Toast.LENGTH_SHORT).show();
+
+                }else{
+                    //user not signed in
+                    startActivityForResult(
+                            AuthUI.getInstance()
+                                    .createSignInIntentBuilder()
+                                    .setIsSmartLockEnabled(false)
+                                    .setAvailableProviders(Arrays.asList(
+                                            new AuthUI.IdpConfig.GoogleBuilder().build(),
+                                            new AuthUI.IdpConfig.FacebookBuilder().build()))
+                                    .build(),
+                            RC_SIGN_IN);
+                }
+
+            }
+        };
+    }
+
+
+    //detaching listener from firebase auth.
+    @Override
+    protected void onPause() {
+        super.onPause();
+        firebaseAuth.removeAuthStateListener(authStateListener);
+    }
+
+    //ataching listener from firebase auth
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        firebaseAuth.addAuthStateListener(authStateListener);
+
     }
 }
