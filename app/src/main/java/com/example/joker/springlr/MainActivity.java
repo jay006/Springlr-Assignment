1605b423ce57521458e4c594c37aaed91832779b
Shriom <shriomtripathi33@gmail.com>
2018-04-21 16:38:20 +0530

    Changes listed below 1.Checked whether user logged in usig facebook or google. 2.Done handling signedIn flow cancelled by the user and showed the respective toast message. 3.Added menu option for the user to signOut.

--- app/src/main/java/com/example/joker/springlr/MainActivity.java
+++ app/src/main/java/com/example/joker/springlr/MainActivity.java
@@ -1,13 +1,18 @@
 package com.example.joker.springlr;
 
+import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.Toast;
 
 import com.firebase.ui.auth.AuthUI;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.UserInfo;
 
 import java.util.Arrays;
 
@@ -22,6 +27,9 @@ public class MainActivity extends AppCompatActivity {
     //auth state listener
     private FirebaseAuth.AuthStateListener authStateListener;
 
+    //UserName
+    private static String userName = "userName";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -35,12 +43,28 @@ public class MainActivity extends AppCompatActivity {
             public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
 
                 FirebaseUser user = firebaseAuth.getCurrentUser();
-                if(user != null){
+                if (user != null) {
                     //user signed in
-                    Toast.makeText(MainActivity.this,"You have logged In.",Toast.LENGTH_SHORT).show();
 
-                }else{
+                    userName = user.getDisplayName();
+
+                    //check whether user logged in using facebook or google.
+                    for (UserInfo userInfo : user.getProviderData()) {
+
+                        if (userInfo.getProviderId().equals("facebook.com")) {
+                            //For linked facebook account
+                            Log.d("provider_info", "User is signed in with Facebook");
+
+                        } else if (userInfo.getProviderId().equals("google.com")) {
+                            //For linked Google account
+                            Log.d("provider_info", "User is signed in with Google");
+                        }
+
+                    }
+
+                } else {
                     //user not signed in
+
                     startActivityForResult(
                             AuthUI.getInstance()
                                     .createSignInIntentBuilder()
@@ -55,7 +79,22 @@ public class MainActivity extends AppCompatActivity {
             }
         };
     }
+    //handling signIn cancelled on backPress.
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
 
+        if (requestCode == RC_SIGN_IN) {
+            if (resultCode == RESULT_OK) {
+                Toast.makeText(MainActivity.this, userName + " logged in.", Toast.LENGTH_SHORT).show();
+
+            } else if (resultCode == RESULT_CANCELED) {
+                Toast.makeText(MainActivity.this, "SignedIn cancelled ", Toast.LENGTH_SHORT).show();
+                finish();
+            }
+        }
+
+    }
 
     //detaching listener from firebase auth.
     @Override
@@ -70,6 +109,27 @@ public class MainActivity extends AppCompatActivity {
         super.onResume();
 
         firebaseAuth.addAuthStateListener(authStateListener);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()){
+            case R.id.signOut:
+                //signOut
+                AuthUI.getInstance().signOut(this);
+                return true;
+
+            default:
+            return super.onOptionsItemSelected(item);
+        }
 
     }
 }
